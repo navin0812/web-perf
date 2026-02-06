f41811ed98d1bf8ed4889873bdd32acee2bad4d2
Navin Joseph <navinj@sph.com.sg>
2026-02-06 14:16:33 +0800

    remove dist files.

--- packages/core/dist/audits/constants.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const WCAG_MAPPINGS: Record<string, {
-    id: string;
-    level: "A" | "AA" | "AAA";
-    name: string;
-    description: string;
-}>;
-export declare const RULE_TO_CATEGORY: Record<string, string>;
-//# sourceMappingURL=constants.d.ts.map
--- packages/core/dist/reporters/json.d.ts
+++
@@ -1,6 +0,0 @@
-import { AuditReport } from "../types.js";
-/**
- * Render audit report as JSON string
- */
-export declare function renderJsonReport(report: AuditReport): string;
-//# sourceMappingURL=json.d.ts.map
